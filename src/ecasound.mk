82249c767713c556479ede170901abab153f7107
Matthias Geier <Matthias.Geier@gmail.com>
2021-05-09 11:48:39 +0200

    autoconf

--- src/ecasound.mk
+++ src/ecasound.mk
@@ -18,6 +18,7 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
+    cd '$(SOURCE_DIR)' && autoconf
     cd '$(BUILD_DIR)' && '$(SOURCE_DIR)'/configure \
         $(MXE_CONFIGURE_OPTS) \
                 --enable-libsamplerate \
